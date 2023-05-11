d366359c422ef914d51ef51c149143024b9fbf5a
Atavaliev Timur <tatavaliev@gmail.com>
2023-05-11 14:31:41 +0600

    Add types to variables

--- task2/board.ts
+++ task2/board.ts
@@ -1,17 +1,19 @@
-const board = document.createElement('div');
+const whiteSquare:string = "  ";
+const blackSquare:string = "██";
+let chessBoard:string = "";
 
-const whiteSquare = "  ";
-const blackSquare = "██";
-
-let chessBoard = "";
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        if (i % 2 !== 0) {
-            j % 2 !== 0 ? chessBoard += whiteSquare : chessBoard += blackSquare;
-        } else {
-            j % 2 !== 0 ? chessBoard += blackSquare : chessBoard += whiteSquare;
+const drawBoard = (size:number = 8) => {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            if (i % 2 !== 0) {
+                j % 2 !== 0 ? chessBoard += whiteSquare : chessBoard += blackSquare;
+            } else {
+                j % 2 !== 0 ? chessBoard += blackSquare : chessBoard += whiteSquare;
+            }
         }
+        chessBoard += "\n";
     }
-    chessBoard += "\n";
+    console.log(chessBoard);
 }
-console.log(chessBoard)
+
+drawBoard(); //by default size value = 8;
